cdb42e485849ae7b455c540cfc27aa9edbf608da
Anastasia Verbitskaya <nastaverb@gmail.com>
2022-10-16 17:58:38 +0300

    Настроить передачу данных в карточки

--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -9,6 +9,7 @@ import RowStyles from '../IngredientRow/IngredientRow.module.css';
 import { Counter } from '@ya.praktikum/react-developer-burger-ui-components';
 
 import Card from '../Card/Card.jsx';
+import data from '../../utils/data.js';
 
 
 
@@ -32,22 +33,37 @@ function BurgerIngredients() {
         <div className={IngredientStyles.BurgerScroll}>
           <h2 className="text text_type_main-medium pb-6">Булки</h2>
           <div className={RowStyles.IngredientRow}>
-            <Card />
-            <Card />
+            <Card image={data[0].image} price={data[0].price} name={data[0].name} />
+            <Card image={data[14].image} price={data[14].price} name={data[14].name} />
           </div>
           <h2 className="text text_type_main-medium pt-10 pb-6">Соусы</h2>
           <div className={RowStyles.IngredientRow}>
-            <Card />
-            <Card />
+            <Card image={data[3].image} price={data[3].price} name={data[3].name} />
+            <Card image={data[5].image} price={data[5].price} name={data[5].name} />
           </div>
           <div className={RowStyles.IngredientRow}>
-            <Card />
-            <Card />
+            <Card image={data[6].image} price={data[6].price} name={data[6].name} />
+            <Card image={data[9].image} price={data[9].price} name={data[9].name} />
           </div>
           <h2 className="text text_type_main-medium pt-10 pb-6">Начинки</h2>
           <div className={RowStyles.IngredientRow}>
-            <Card />
-            <Card />
+            <Card image={data[1].image} price={data[1].price} name={data[1].name} />
+            <Card image={data[2].image} price={data[2].price} name={data[2].name} />
+          </div>
+          <div className={RowStyles.IngredientRow}>
+            <Card image={data[4].image} price={data[4].price} name={data[4].name} />
+            <Card image={data[7].image} price={data[7].price} name={data[7].name} />
+          </div>
+          <div className={RowStyles.IngredientRow}>
+            <Card image={data[8].image} price={data[8].price} name={data[8].name} />
+            <Card image={data[10].image} price={data[10].price} name={data[10].name} />
+          </div>
+          <div className={RowStyles.IngredientRow}>
+            <Card image={data[11].image} price={data[11].price} name={data[11].name} />
+            <Card image={data[12].image} price={data[12].price} name={data[12].name} />
+          </div>
+          <div className={RowStyles.IngredientRow}>
+            <Card image={data[13].image} price={data[13].price} name={data[13].name} />
           </div>
         </div>
       </section>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,19 +4,20 @@ import ReactDOM from 'react-dom'
 import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import BigBun from '../../images/bunBig.png';
 import { Counter } from '@ya.praktikum/react-developer-burger-ui-components';
+import { isPropertySignature } from 'typescript';
 
 
 
-function Card() {
+function Card(props) {
   return (
     <div className='' style={{ position: 'relative', width: '272px' }}>
-      <img src={BigBun} />
+      <img src={props.image} />
       <Counter count={1} size="default" />
       <div className="pt-1 pb-1" style={{ display: 'flex', justifyContent: 'center', gap: '9px' }}>
-        <span className="text text_type_digits-default">20</span>
+        <span className="text text_type_digits-default">{props.price}</span>
         <CurrencyIcon type="primary" />
       </div>
-      <p className="text text_type_main-default pb-6">Флюоресцентная булка R2-D3</p>
+      <p className="text text_type_main-default pb-6">{props.name}</p>
 
     </div>
   );
