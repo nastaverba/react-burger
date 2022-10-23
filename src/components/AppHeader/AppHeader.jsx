380f39864bed8aec8857ae1d4a9e15e3635a68db
Anastasia Verbitskaya <nastaverb@gmail.com>
2022-10-23 17:21:52 +0300

    Удалить лишнюю обертку в AppHeader

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -7,31 +7,29 @@ import AppHeaderStyles from "./AppHeader.module.css";
 
 function AppHeader() {
   return (
-    <>
-      <header className={AppHeaderStyles.AppHeader}>
-        <div className={AppHeaderStyles.MenuContainer}>
-          <a className={AppHeaderStyles.MenuItem} href="/#">
-            <div className={AppHeaderStyles.MenuItemIcon}>
-              <BurgerIcon type="primary" />
-            </div>
-            <span className="text text_type_main-default">Конструктор</span>
-          </a>
-          <a className={AppHeaderStyles.MenuItemSecondary} href="/#">
-            <div className={AppHeaderStyles.MenuItemIcon}>
-              <ListIcon type="secondary" />
-            </div>
-            <span className="text text_type_main-default">Лента заказов</span>
-          </a>
-        </div>
-        <Logo />
+    <header className={AppHeaderStyles.AppHeader}>
+      <div className={AppHeaderStyles.MenuContainer}>
+        <a className={AppHeaderStyles.MenuItem} href="/#">
+          <div className={AppHeaderStyles.MenuItemIcon}>
+            <BurgerIcon type="primary" />
+          </div>
+          <span className="text text_type_main-default">Конструктор</span>
+        </a>
         <a className={AppHeaderStyles.MenuItemSecondary} href="/#">
           <div className={AppHeaderStyles.MenuItemIcon}>
-            <ProfileIcon type="secondary" />
+            <ListIcon type="secondary" />
           </div>
-          <span className="text text_type_main-default">Личный кабинет</span>
+          <span className="text text_type_main-default">Лента заказов</span>
         </a>
-      </header>
-    </>
+      </div>
+      <Logo />
+      <a className={AppHeaderStyles.MenuItemSecondary} href="/#">
+        <div className={AppHeaderStyles.MenuItemIcon}>
+          <ProfileIcon type="secondary" />
+        </div>
+        <span className="text text_type_main-default">Личный кабинет</span>
+      </a>
+    </header>
   );
 }
 
